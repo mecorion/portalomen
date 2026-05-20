ac86a47ae53a164e883b6dc17e1584d2c5a2331a
Mecorion <mecorion@atomicmail.io>
2026-05-21 02:08:01 +0300

    Доработка для отображения нескольких страниц

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+
 import DashboardView from './views/dashboard/DashboardView.vue'
+import UIShowcase from './views/ui/UIShowcase.vue'
+
+const currentHash = ref(window.location.hash || '#/')
+
+const currentView = computed(() => {
+  return currentHash.value === '#/ui' ? UIShowcase : DashboardView
+})
+
+function syncHash() {
+  currentHash.value = window.location.hash || '#/'
+}
+
+onMounted(() => {
+  window.addEventListener('hashchange', syncHash)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('hashchange', syncHash)
+})
 </script>
 
 <template>
-  <DashboardView />
+  <component :is="currentView" />
 </template>
